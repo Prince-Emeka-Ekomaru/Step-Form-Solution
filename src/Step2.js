fcc484a5c7b24eea894d6ed1d9ca8af3328456af
Prince Emeka Ekomaru <princeekomaru@gmail.com>
2021-01-16 15:06:12 +0100

    Initialize project using Create React App

---
+++ src/Step2.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Step2(props) {
+  return (
+    <div>
+      <p>Email: <input name="email" /></p>
+      <p>Phone: <input name="Phone" /></p>
+    </div>
+  );
+}
+
+export default Step2;
